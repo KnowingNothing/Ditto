243c8a4f677823277096e732d6f6dc9731c48530
ppppaidy <ppppaidy@pku.edu.cn>
2021-09-29 11:07:28 +0800

    modify s -> r

--- python_test/region/test_create_region.py
+++ python_test/region/test_create_region.py
@@ -4,4 +4,4 @@ import ditto
 A = tvm.te.placeholder([6, 6], name="A")
 C = tvm.te.compute([6, 6], lambda i, j: A[i, j] + 1, name="C")
 
-s = ditto.region.create_region(C.op)
+r = ditto.region.create_region(C.op)
