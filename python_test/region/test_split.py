27ee00d7efe0a889c19904d1c22d0be5a882dd50
ppppaidy <ppppaidy@pku.edu.cn>
2021-09-29 11:07:47 +0800

    add test_split.py

---
+++ python_test/region/test_split.py
@@ -0,0 +1,10 @@
+import tvm
+import ditto
+
+A = tvm.te.placeholder([6, 6], name="A")
+C = tvm.te.compute([6, 6], lambda i, j: A[i, j] + 1, name="C")
+
+r = ditto.region.create_region(C.op)
+
+i, j = r.op.axis
+r.split(i, factor=1)
