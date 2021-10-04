f3dfca99df74b9be5e83e249753a418473abcd76
ppppaidy <ppppaidy@pku.edu.cn>
2021-10-04 12:48:29 +0800

    add test_slice.py

---
+++ python_test/region/test_slice.py
@@ -0,0 +1,11 @@
+import tvm
+import ditto
+
+A = tvm.te.placeholder([6, 6], name="A")
+C = tvm.te.compute([6, 6], lambda i, j: A[i, j] + 1, name="C")
+
+s = ditto.region.create_region_schedule(C.op)
+
+i, j = s[C].op.axis
+
+s.slice(C, i, slice_point=4)
