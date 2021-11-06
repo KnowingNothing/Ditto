9ee9ae6088221b57057f1b8604be72b2c1a5995e
KnowingNothing <zhengsizeMax@look.com>
2021-11-06 13:53:41 +0800

    fix(runtime): remove std::cout

--- python/ditto/auto_compute/nn/model/__init__.py
+++ python/ditto/auto_compute/nn/model/__init__.py
@@ -15,7 +15,11 @@ all_models = {
     "lenet": LeNet5,
     "lltm": LLTM,
     "MI": MI_LSTM,
+    "res18": resnet18,
+    "res34": resnet34,
     "res50": resnet50,
+    "res101": resnet101,
+    "res152": resnet152,
     "sc": SCRNN,
     "shuffle": ShuffleNet,
     "sub": subLSTM
@@ -27,7 +31,11 @@ model_inputs_shape = {
     "lenet": [[1, 1, 32, 32]],
     "lltm": [[1, 28 * 28], [1, 128], [1, 128]],
     "MI": [[1, 28 * 28], [1, 1024], [1, 1024]],
+    "res18": [[1, 3, 224, 224]],
+    "res34": [[1, 3, 224, 224]],
     "res50": [[1, 3, 224, 224]],
+    "res101": [[1, 3, 224, 224]],
+    "res152": [[1, 3, 224, 224]],
     "sc": [[1, 28 * 28], [1, 128], [1, 64]],
     "shuffle": [[1, 3, 224, 224]],
     "sub": [[1, 28 * 28], [1, 128], [1, 128]]
